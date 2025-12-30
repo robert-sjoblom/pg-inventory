5d03d0be14e950fabe51e7179be4ca1066ca66e0
Robert Sjöblom <robert.sjoblom@gmail.com>
2025-12-30 23:38:04 +0100

    Restructure for multiple binaries

---
+++ cmd/extractor/main.go
@@ -0,0 +1,15 @@
+// Extractor - Connects to PostgreSQL instances and extracts inventory data from each database.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("PostgreSQL Inventory Extractor")
+	// TODO: Load config
+	log.Println("Extractor service starting...")
+	os.Exit(0)
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,3 @@
 module github.com/robert-sjoblom/pg-inventory
 
 go 1.25
-
-require (
-	github.com/jmoiron/sqlx v1.4.0
-	github.com/lib/pq v1.10.9
-	github.com/spf13/cobra v1.10.1
-)
-
-require (
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
-)

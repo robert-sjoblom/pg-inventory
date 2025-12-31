e4cded5f24422aa2d2fe0e9504843bc3fc9f139d
Robert Sjöblom <robert.sjoblom@gmail.com>
2026-01-01 00:34:28 +0100

    Add testutil DialBufconn func

---
+++ internal/testutil/grpc.go
@@ -0,0 +1,45 @@
+// Package testutil contains test utilities like bufconn or test containers
+package testutil
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	extractorv1 "github.com/robert-sjoblom/pg-inventory/gen/extractor/v1"
+	"github.com/robert-sjoblom/pg-inventory/internal/extractor"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+// DialBufconn creates an in-memory gRPC connection for testing.
+// It returns a gRPC ClientConn and a cleanup function.
+func DialBufconn(t *testing.T) *grpc.ClientConn {
+	t.Helper()
+
+	lis := bufconn.Listen(1024 * 1024)
+	s := grpc.NewServer()
+
+	extractorv1.RegisterExtractorServiceServer(s, &extractor.Server{})
+
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
+
+	conn, err := grpc.NewClient("", grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+		return lis.Dial()
+	}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("failed to dial bufconn: %v", err)
+	}
+
+	t.Cleanup(func() {
+		_ = conn.Close()
+		s.Stop()
+	})
+
+	return conn
+}

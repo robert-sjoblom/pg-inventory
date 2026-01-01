1d2ac72701a8e4b99b9f1e638489ca6046c50ecb
Robert Sjöblom <robert.sjoblom@gmail.com>
2026-01-01 22:33:22 +0100

    Wire store to extractor main
    Remove server_test.go; it was useful when we had hardcoded data,
    but now it's redundant; store test already covers query logic.
    We'll add integration tests later.

--- internal/extractor/server.go
+++ internal/extractor/server.go
@@ -5,26 +5,33 @@ import (
 	"context"
 
 	extractorv1 "github.com/robert-sjoblom/pg-inventory/gen/extractor/v1"
+	"github.com/robert-sjoblom/pg-inventory/internal/extractor/store"
 )
 
 type Server struct {
 	extractorv1.UnimplementedExtractorServiceServer
+	store *store.Store
+}
+
+func NewServer(s *store.Store) *Server {
+	return &Server{
+		store: s,
+	}
 }
 
 func (s *Server) ListDatabases(ctx context.Context, req *extractorv1.ListDatabasesRequest) (*extractorv1.ListDatabasesResponse, error) {
-	databases := []*extractorv1.Database{
-		{
-			Name: "postgres",
-			Oid:  1,
-		},
-		{
-			Name: "database_one",
-			Oid:  2,
-		},
-		{
-			Name: "database_two",
-			Oid:  1,
-		},
+	dbs, err := s.store.ListDatabases(ctx)
+	if err != nil {
+		return nil, err
 	}
+
+	databases := make([]*extractorv1.Database, 0, len(dbs))
+	for _, db := range dbs {
+		databases = append(databases, &extractorv1.Database{
+			Oid:  db.Oid,
+			Name: db.Name,
+		})
+	}
+
 	return &extractorv1.ListDatabasesResponse{Databases: databases}, nil
 }
---
+++ internal/extractor/types/types.go
@@ -0,0 +1,7 @@
+// Package types holds domain types for the extractor
+package types
+
+type Database struct {
+	Name string
+	Oid  uint32
+}
